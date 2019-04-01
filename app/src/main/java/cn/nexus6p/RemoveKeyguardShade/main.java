64061e70aef6810024aa50bf47aebaeb7ffe97df
Neuron <liziyuan123456@yeah.net>
2019-04-01 21:32:33 +0800

    release  1.1
    尝试移除状态栏遮罩

--- app/src/main/java/cn/nexus6p/RemoveKeyguardShade/main.java
+++ app/src/main/java/cn/nexus6p/RemoveKeyguardShade/main.java
@@ -1,13 +1,17 @@
 package cn.nexus6p.RemoveKeyguardShade;
 
+import android.content.res.XResources;
+import android.graphics.Color;
+
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.setFloatField;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class main implements IXposedHookLoadPackage {
+public class main implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals("com.android.systemui"))
 			return;
@@ -21,4 +25,9 @@ public class main implements IXposedHookLoadPackage {
         });
 
 	}
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        XResources.setSystemWideReplacement("android","color","system_bar_background_semi_transparent", 0);
+    }
 }
