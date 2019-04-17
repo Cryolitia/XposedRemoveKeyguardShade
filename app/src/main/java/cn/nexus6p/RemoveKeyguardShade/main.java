da0bda58041abf6b7be3a8f95eb437d47b8d0442
Neuron <liziyuan123456@yeah.net>
2019-04-17 10:59:32 +0800

    release 1.3
    最后一次尝试解决一加不生效问题

--- app/src/main/java/cn/nexus6p/RemoveKeyguardShade/main.java
+++ app/src/main/java/cn/nexus6p/RemoveKeyguardShade/main.java
@@ -1,43 +1,67 @@
 package cn.nexus6p.RemoveKeyguardShade;
 
-import android.content.res.XResources;
-import android.graphics.Color;
-
-import static de.robv.android.xposed.XposedHelpers.findAndHookConstructor;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.setFloatField;
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class main implements IXposedHookLoadPackage, IXposedHookZygoteInit {
-	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+public class main implements IXposedHookLoadPackage{
+	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals("com.android.systemui"))
 			return;
 
         final Class clazz = XposedHelpers.findClass("com.android.systemui.statusbar.phone.ScrimController",lpparam.classLoader);
-        /*findAndHookMethod(clazz, "updateScrimKeyguard", new XC_MethodHook() {
+
+        try {
+        findAndHookMethod(clazz, "updateScrimKeyguard", new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
                 setFloatField(param.thisObject,"mScrimBehindAlphaKeyguard",1.0E-4f);
             }
-        });*/
-        XposedBridge.hookAllConstructors(clazz, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-                setFloatField(param.thisObject,"mScrimBehindAlphaKeyguard",1.0E-4f);
-            }
-        });
-
+            });
+        }
+        catch (Throwable t) {
+            t.printStackTrace();
+        }
+        try {
+            XposedBridge.hookAllConstructors(clazz, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    setFloatField(param.thisObject,"mScrimBehindAlphaKeyguard",1.0E-4f);
+                }
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    setFloatField(param.thisObject,"mScrimBehindAlphaKeyguard",1.0E-4f);
+                }
+            });
+        }
+        catch (Throwable t) {
+            t.printStackTrace();
+        }
+        try {
+            XposedHelpers.findAndHookMethod(clazz, "scheduleUpdate", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) {
+                    Object[] states = (Object[]) XposedHelpers.callStaticMethod(XposedHelpers.findClass("com.android.systemui.statusbar.phone.ScrimState", lpparam.classLoader), "values");
+                    final float alpha = 1.0E-4f;
+                    for (Object state : states) {
+                        XposedHelpers.callMethod(state, "setScrimBehindAlphaKeyguard", alpha);
+                    }
+                }
+            });
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
 	}
 
-    @Override
+    /*@Override
     public void initZygote(StartupParam startupParam) throws Throwable {
         XResources.setSystemWideReplacement("android","color","system_bar_background_semi_transparent", 0);
-    }
+    }*/
 }
